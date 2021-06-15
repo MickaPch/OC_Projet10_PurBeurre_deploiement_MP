79e06d80fca0383292210158d06d9dea1ffced56
MickaP <mickael.perchet@gmail.com>
2021-06-15 16:44:25 +0200

    static_root

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -169,11 +169,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/ybwe3630/purbeurre.mickapr.fr/static"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/"),
